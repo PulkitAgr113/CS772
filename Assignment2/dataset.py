fa8501eab7300fade54587d024a8de37fc4defe5
Pulkit <iampulkit2002@gmail.com>
2023-03-08 14:01:43 +0530

    Training for FFNN

--- Assignment2/dataset.py
+++ Assignment2/dataset.py
@@ -12,19 +12,22 @@ def create_k_folds(tagged_sentence, k=5, seed=0):
         folds[i % k].append(sentence)
     return folds
 
-tagged_sentences = nltk.corpus.brown.tagged_sents(tagset='universal')
+def data():
+    tagged_sentences = nltk.corpus.brown.tagged_sents(tagset='universal')
 
-tagset = set([tag for sentence in tagged_sentences for word, tag in sentence])
-tagset = sorted(list(tagset))
-tagidxdict = {tag: idx for idx, tag in enumerate(tagset)}
-num_tags = len(tagset)
+    tagset = set([tag for sentence in tagged_sentences for word, tag in sentence])
+    tagset = sorted(list(tagset))
+    tagidxdict = {tag: idx for idx, tag in enumerate(tagset)}
 
-sentences = []
-correct_tags = []
-for sentence in tagged_sentences:
-    unzipped_sentence = list(zip(*sentence))
-    sentences.append(list(map(lambda x: x.lower(), unzipped_sentence[0])))
-    correct_tags.append(list(unzipped_sentence[1]))
+    sentences = []
+    correct_tags = []
+    for sentence in tagged_sentences:
+        unzipped_sentence = list(zip(*sentence))
+        sentences.append(list(map(lambda x: x.lower(), unzipped_sentence[0])))
+        correct_tags.append(list(unzipped_sentence[1]))
 
-folded_sentences = create_k_folds(sentences, seed=0)
-folded_correct_tags = create_k_folds(correct_tags, seed=0)
+    folded_sentences = create_k_folds(sentences)
+    folded_correct_tags = create_k_folds(correct_tags)
+    print("Folds created")
+
+    return tagidxdict, folded_sentences, folded_correct_tags
--- Assignment2/ffnn.py
+++ Assignment2/ffnn.py
@@ -1,8 +1,10 @@
-# from gensim.models import KeyedVectors
+from gensim.models import KeyedVectors
 import torch
 from torch import nn
-
-# word_vectors_google = KeyedVectors.load('vectors.bin')
+from torch.utils.data import Dataset, DataLoader
+import numpy as np
+from tqdm import tqdm
+from dataset import data
 
 class NeuralNet(nn.Module):
     def __init__(self, input_size, hidden_size, num_classes):
@@ -19,14 +21,65 @@ class NeuralNet(nn.Module):
         out = self.l2(out)
         out = self.softmax(out)
         return out
+
+class dataloader(Dataset):
+    def __init__(self, folded_train_sents, folded_train_correct_tags, prev_words, tagidxdict, word_vectors_google, input_size, num_classes):
+        inp_out = []
+        for train_sents, train_correct_tags in zip(folded_train_sents, folded_train_correct_tags):
+            for sent, correct_tags in zip(train_sents, train_correct_tags):
+                ind = 0
+                for ind in range(len(sent)):
+                    elem = np.zeros(input_size + num_classes)
+                    corr_tag = correct_tags[ind]
+                    idx = tagidxdict[corr_tag]
+                    elem[input_size+idx] = 1
+                    for word in range(max(ind-prev_words, 0), ind+1):
+                        if sent[word] in word_vectors_google:
+                            elem[:input_size] += word_vectors_google[sent[word]]
+                    inp_out.append(elem)
+        
+        self.inp_out = inp_out
+                
+    def __len__(self):
+        return len(self.inp_out)
     
-input_size = 300
-hidden_size = 64
-num_classes = 12
-epochs = 2
-lr = 0.001
+    def __getitem__(self, idx):
+        return self.inp_out[idx]
 
-model = NeuralNet(input_size, hidden_size, num_classes)
-loss = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr = lr)
+def run(model, criterion, optimizer, train_sents, train_correct_tags, tagidxdict, word_vectors_google, batch_size, input_size, num_classes, prev_words = 4):
+    train_vectors = dataloader(train_sents, train_correct_tags, prev_words, tagidxdict, word_vectors_google, input_size, num_classes)
+    train_dataloader = DataLoader(train_vectors, batch_size = batch_size, shuffle=True)
+    error = 0
+    for idx,item in enumerate(tqdm(train_dataloader)):
+        input_vec = item[:input_size]
+        output_vec = item[input_size:]
+        pred_vec = model.forward(input_vec)
+        loss = criterion(pred_vec, output_vec)
+        error += loss.item()
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
+    return error
+
+if __name__ == '__main__':
+    prev_words = 4
+    input_size = 300
+    hidden_size = 64
+    num_classes = 12
+    epochs = 2
+    lr = 0.001
+    batch_size = 256
+
+    model = NeuralNet(input_size, hidden_size, num_classes)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr = lr)
+
+    word_vectors_google = KeyedVectors.load('vectors.bin')
+    print("Word vectors loaded")
+
+    tagidxdict, folded_sentences, folded_correct_tags = data()
+
+    for epoch in range(epochs):
+        error = run(model, criterion, optimizer, folded_sentences, folded_correct_tags, tagidxdict, word_vectors_google, batch_size, input_size, num_classes)
+        print(f"Epoch {epoch} completed. Error: {error}")
